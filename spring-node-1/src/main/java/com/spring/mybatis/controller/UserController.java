5ec2550e929cf85a0d1741596c8bc74ad7c8ca78
余昊 <yuhao@lifengyun.com>
2017-02-17 19:44:31 +0800

    node1可发出代理请求

--- spring-node-1/src/main/java/com/spring/mybatis/controller/UserController.java
+++ spring-node-1/src/main/java/com/spring/mybatis/controller/UserController.java
@@ -11,12 +11,17 @@ import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.pac4j.cas.profile.CasProxyProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.mybatis.service.UserService;
+
+import io.buji.pac4j.subject.Pac4jPrincipal;
 
 @Controller
 @RequestMapping("/users")
@@ -35,19 +40,21 @@ public class UserController {
 		return "../index";
 	}
 	
-	/*@RequestMapping("/secure/makeRequest")
+	@RequestMapping("/callbyproxy")
     public void makeRequest(HttpServletResponse response) throws IOException {
         HttpClient client = new HttpClient();
         String targetUrl = "http://localhost:9090/node2/users/secureRequest.htm";
-        CasAuthenticationToken token = (CasAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-        String ticket = token.getAssertion().getPrincipal().getProxyTicketFor(targetUrl);
+        PrincipalCollection principals = SecurityUtils.getSubject().getPrincipals(); 
+        Pac4jPrincipal principal =  principals.byType(Pac4jPrincipal.class).iterator().next(); 
+        CasProxyProfile profile = (CasProxyProfile)principal.getProfile();
+        String ticket = profile.getProxyTicketFor(targetUrl); 
         HttpMethod m = new GetMethod(targetUrl+"?ticket=" + ticket);
         m.setFollowRedirects(false);
         client.executeMethod(m);
 
         PrintWriter out = response.getWriter();
         renderResponse(m, out);
-    }*/
+    }
 
     private void renderResponse(HttpMethod m, PrintWriter out) throws IOException {
         out.println("Response For: " + m.getPath());
